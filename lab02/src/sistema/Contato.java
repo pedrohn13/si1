cf3860e5146ece3671f3b0caa0b4e2a8c62f5c62
Pedro Henriques <pedrohn13@gmail.com>
2013-06-19 21:30:35 -0300

    Testes Passando do Inserir

--- lab02/src/sistema/Contato.java
+++ lab02/src/sistema/Contato.java
@@ -1,14 +1,16 @@
 package sistema;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class Contato {
+public class Contato implements Comparable<Contato> {
 
 	private String nome;
 	private List<Telefone> telefones;
 
 	public Contato(String nome, int telefone) {
 		this.nome = nome;
+		this.telefones = new ArrayList<Telefone>();
 		this.telefones.add(new Telefone(telefone));
 	}
 
@@ -26,5 +28,35 @@ public class Contato {
 
 	public void setTelefones(List<Telefone> telefones) {
 		this.telefones = telefones;
+	}
+
+	@Override
+	public int compareTo(Contato outroContato) {
+		return this.nome.compareTo(outroContato.getNome());
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Contato other = (Contato) obj;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		return true;
 	}
 }
